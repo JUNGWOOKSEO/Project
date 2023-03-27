5b0d5a7944477e853bd52b53aa3b2c28355e6b43
sunghwanh <hsung1010@naver.com>
2023-03-27 20:43:39 +0900

    tt

--- bank/src/main/java/pack/controller/CustomerController.java
+++ bank/src/main/java/pack/controller/CustomerController.java
@@ -3,68 +3,18 @@ package pack.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
-public class CustomerController implements WebMvcConfigurer{
-	 //이거이거
-//	 @GetMapping("/index")
-//	    public String main(){
-//	        return "index";
-//	    }
-	@GetMapping("/login")
-	public String loginpage(Model model) {
-		return "html.login/login";
+public class CustomerController{
+	@GetMapping("/mailconsult")
+	public String mailconsult(Model model) {
+		return "/html.customer/mailconsult";
 	}
 	
-	@GetMapping("prodtmain")
-	public String productmain() {
-		return "html.product/prodtmain";
+	@GetMapping("/chatconsult")
+	public String mailconsult() {
+		return "/html.customer/chatconsult";
 	}
 
-//	@GetMapping("faq")
-//	public String fPage(Model model) {
-//		return "faq";
-//	}
-//
-//	@PostMapping("faq")
-//	public String fpPage(Model model) {
-//		return "faq";
-//	}
-//
-//	@GetMapping("main")
-//	public String mainPage(Model model) {
-//		return "main";
-//	}
-//
-//	@GetMapping("history")
-//	public String historyPage(Model model) {
-//		return "history";
-//	}
-//
-//	
-//	@GetMapping("chatbot")
-//	public String chatPage(Model model) {
-//		return "redirect:/chatbot";
-//	}
-//
-//	@GetMapping("terms")
-//	public String termsPage(Model model) {
-//		return "terms";
-//	}
-//	@GetMapping("mail")
-//	public String mailPage(Model model) {
-//		return "mail";
-//	}
-//	@GetMapping("chatmain")
-//	public String gogekPage(Model model) {
-//		return "chatmain";
-//	}
-//
-//	@GetMapping("userHeader")
-//	public String headerPage(Model model) {
-//		return "userHeader";
-//	}
-//	
-	
+
 }
